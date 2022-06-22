e60a5a6ab50ecc9d07f3cf32da83f42810e89ccb
Hades-Bot-MD <108025565+Yovanihades1212@users.noreply.github.com>
2022-06-22 18:36:02 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,76 @@
+global.DeveloperMode = 'false' //true Or false
+global.linkGC = ['https://chat.whatsapp.com/CG5ZPcJ22fL7QjNRzjguD0'] // No tiene utilidad 
+global.channelYT = ['https://chat.whatsapp.com/CG5ZPcJ22fL7QjNRzjguD0'] // No tiene utilidad
+global.owner = ['5212411347465'] //Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
+global.mods = ['5212412377467'] //No tiene utilidad
+global.prems = ['521211719888' , '5212411347465', '5212412377467'] //Podra usar algunos comandos del apartado de propietario
+
+//Lo siguiente no tiene nada que ver con el funcionamiento del Bot:
+//Nota: Puedes contactarme si necesitas ayuda con algo al +5219996125657 (este es mi numero personal/real bloqueo personas que no sean temas de la instalación)
+//=> Solo dudas sobre la instalación, temas generales contactarme al +52 241 373 7467 o al +52 241 134 7465
+//=> NO ayudo a hacer Bots, ni crearlos, ni editarlos 
+//- Haré tutoriales de como editar el Bot, los videos los podran encontrar en mi canal de YouTube en https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA
+
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  amel: 'https://melcanz.com',
+  bx: 'https://bx-hunter.herokuapp.com',
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  nzcha: 'http://nzcha-apii.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  fdci: 'https://api.fdci.se',
+  dzx: 'https://api.dhamzxploit.my.id',
+  bsbt: 'https://bsbt-api-rest.herokuapp.com',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.me',
+  hardianto: 'https://hardianto.xyz',
+  pencarikode: 'https://api.chipa.xyz', 
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  adiisus: 'https://adiixyzapi.herokuapp.com',
+  lol: 'https://api.lolhuman.xyz',
+  pencarikode: 'https://pencarikode.xyz',
+  Velgrynd: 'https://velgrynd.herokuapp.com',
+  rey: 'https://server-api-rey.herokuapp.com',
+  shadow: 'https://api.reysekha.xyz',
+  neoxr: 'https://api.neoxr.eu.org',
+  apialc: 'https://api-alc.herokuapp.com'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api-alc.herokuapp.com': '',
+  'https://api.reysekha.xyz': 'apirey',
+  'https://melcanz.com': 'F3bOrWzY',
+  'https://bx-hunter.herokuapp.com': 'Ikyy69',
+  'https://api.xteam.xyz': '5bd33b276d41d6b4',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://bsbt-api-rest.herokuapp.com': 'benniismael',
+  'https://api.zeks.me': 'apivinz',
+  'https://hardianto.xyz': 'hardianto',
+  'https://api.neoxr.eu.org': 'yntkts',
+  'https://api.chipa.xyz': 'pais', 
+  'https://leyscoders-api.herokuapp.com': 'MIMINGANZ', 
+  'https://server-api-rey.herokuapp.com': 'apirey',
+  'https://api.lolhuman.xyz': '9b817532fadff8fc7cb86862'
+}
+
+// Sticker WM
+global.packname = '❍͜͡➣𝐘𝐎𝐕𝐀𝐍𝐈❍͜͡➣'
+global.author = '?????????_???????'
+
+
+global.wm = '?????????_???????'
+global.wait = '*Espere un momento..*'
+
+
+global.multiplier = 69 // The higher, The harder levelup
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
